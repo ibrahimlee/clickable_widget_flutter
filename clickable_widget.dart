c58b4a780c071fabc8afbbd1a91b361f2e6bc7f3
ibrahimlee <33786727+ibrahimlee@users.noreply.github.com>
2022-09-21 18:41:57 +0400

    Update clickable_widget.dart

--- clickable_widget.dart
+++ clickable_widget.dart
@@ -4,6 +4,9 @@ class ClickableWidget extends StatelessWidget {
   final Widget child;
   final GestureTapCallback onTap;
   final double elevation;
+  final Color? shadowColor;
+  final double blurRadius;
+  final double spreadRadius;
   final BorderRadius? borderRadius;
   final Color color;
 
@@ -11,21 +14,37 @@ class ClickableWidget extends StatelessWidget {
       {required this.child,
       required this.onTap,
       this.elevation = 0,
+      this.shadowColor,
+      this.blurRadius = 0.0,
+      this.spreadRadius = 0.0,
       this.color = Colors.transparent,
       this.borderRadius,
       super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: color,
-      shadowColor: Colors.grey.withOpacity(0.2),
-      elevation: elevation,
-      borderRadius: borderRadius,
-      child: InkWell(
+    return Container(
+      decoration: BoxDecoration(
         borderRadius: borderRadius,
-        onTap: onTap,
-        child: child,
+        boxShadow: [
+          BoxShadow(
+            color: shadowColor ?? Colors.transparent,
+            blurRadius: blurRadius,
+            spreadRadius: spreadRadius,
+            offset: const Offset(0, 0),
+          )
+        ],
+      ),
+      child: Material(
+        color: color,
+        shadowColor: shadowColor,
+        elevation: elevation,
+        borderRadius: borderRadius,
+        child: InkWell(
+          borderRadius: borderRadius,
+          onTap: onTap,
+          child: child,
+        ),
       ),
     );
   }
