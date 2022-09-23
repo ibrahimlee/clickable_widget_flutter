b9a374fad1b415884aaa8a7bb4363282630b3af2
ibrahimlee <33786727+ibrahimlee@users.noreply.github.com>
2022-09-23 17:47:27 +0400

    Update clickable_widget.dart
    Theme color added

--- clickable_widget.dart
+++ clickable_widget.dart
@@ -9,6 +9,8 @@ class ClickableWidget extends StatelessWidget {
   final double spreadRadius;
   final BorderRadius? borderRadius;
   final Color color;
+  final Color? highlightColor;
+  final bool hasChildClick;
 
   const ClickableWidget(
       {required this.child,
@@ -18,34 +20,60 @@ class ClickableWidget extends StatelessWidget {
       this.blurRadius = 0.0,
       this.spreadRadius = 0.0,
       this.color = Colors.transparent,
+      this.highlightColor,
       this.borderRadius,
+      this.hasChildClick = false,
       super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        borderRadius: borderRadius,
-        boxShadow: [
-          BoxShadow(
-            color: shadowColor ?? Colors.transparent,
-            blurRadius: blurRadius,
-            spreadRadius: spreadRadius,
-            offset: const Offset(0, 0),
-          )
-        ],
-      ),
-      child: Material(
-        color: color,
-        shadowColor: shadowColor,
-        elevation: elevation,
-        borderRadius: borderRadius,
-        child: InkWell(
+        decoration: BoxDecoration(
           borderRadius: borderRadius,
-          onTap: onTap,
-          child: child,
+          boxShadow: [
+            BoxShadow(
+              color: shadowColor ?? Colors.transparent,
+              blurRadius: blurRadius,
+              spreadRadius: spreadRadius,
+              offset: const Offset(0, 0),
+            )
+          ],
         ),
-      ),
-    );
+        child: Material(
+            color: color,
+            shadowColor: shadowColor,
+            elevation: elevation,
+            clipBehavior: Clip.hardEdge,
+            borderRadius: borderRadius,
+            child: hasChildClick
+                ? InkWell(
+                    highlightColor: highlightColor?.withOpacity(0.1) ??
+                        Theme.of(context).highlightColor,
+                    splashColor: highlightColor?.withOpacity(0.3) ??
+                        Theme.of(context).splashColor,
+                    borderRadius: borderRadius,
+                    onTap: onTap,
+                    child: child,
+                  )
+                : Stack(
+                    fit: StackFit.passthrough,
+                    clipBehavior: Clip.hardEdge,
+                    children: [
+                      child,
+                      Positioned.fill(
+                        child: Material(
+                            color: Colors.transparent,
+                            child: InkWell(
+                              highlightColor:
+                                  highlightColor?.withOpacity(0.1) ??
+                                      Theme.of(context).highlightColor,
+                              splashColor: highlightColor?.withOpacity(0.3) ??
+                                  Theme.of(context).splashColor,
+                              borderRadius: borderRadius,
+                              onTap: onTap,
+                            )),
+                      ),
+                    ],
+                  )));
   }
 }
